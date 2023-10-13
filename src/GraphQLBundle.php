ff7663fd78a1cdcb74a990af4f2e0de8fbcf0476
Matěj Votruba <fsfmatej@gmail.com>
2023-10-13 15:54:00 +0200

    Fix missing argument for GraphQLConfigureCommand

--- src/GraphQLBundle.php
+++ src/GraphQLBundle.php
@@ -4,9 +4,11 @@ namespace Youshido\GraphQLBundle;
 
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\EventDispatcher\DependencyInjection\RegisterListenersPass;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Youshido\GraphQLBundle\DependencyInjection\Compiler\GraphQlCompilerPass;
+use Youshido\GraphQLBundle\DependencyInjection\GraphQLExtension;
 
 class GraphQLBundle extends AbstractBundle
 {
@@ -23,6 +25,15 @@ class GraphQLBundle extends AbstractBundle
             ),
             PassConfig::TYPE_BEFORE_REMOVING
         );
+    }
+
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        if (null === $this->extension) {
+            $this->extension = new GraphQLExtension();
+        }
+
+        return $this->extension;
     }
 
     public function getPath(): string
